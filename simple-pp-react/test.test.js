cc45dc97080d894bf1eb414c7e7cd2dc998a630a
Chris Rehm <cre@toolmaker.de>
2025-05-09 13:27:48 +0200

    wip 2+2 1327

--- simple-pp-react/test.test.js
+++ simple-pp-react/test.test.js
@@ -1,3 +1,7 @@
+import { expect } from "chai";
+
 describe("first test see if we can do aything ", () => {
-  it("runs a blank test ", () => {});
+  it("runs a blank test ", () => {
+    expect(2 + 2).to.be(4);
+  });
 });
