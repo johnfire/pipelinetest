076be6c5de0040b5d150a0e061d9c750c143fc03
Chris Rehm <cre@toolmaker.de>
2025-05-09 14:05:14 +0200

    wip 1405

--- imports/api/text3.test.js
+++ imports/api/text3.test.js
@@ -1,7 +1,9 @@
 import { expect } from "chai";
+import { assert } from "chai";
 
 describe("333 first test see if we can do aything ", function () {
   it("333 runs a blank test ", function () {
-    expect(2 + 2).to.be(4);
+    assert.strictEqual(2 + 3, 5);
+    // expect(2 + 2).to.be(4);
   });
 });
