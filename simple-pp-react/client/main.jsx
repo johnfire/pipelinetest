23cf245e0eb78582325678e62554fd2234c94e9d
Chris Rehm <cre@toolmaker.de>
2025-05-09 12:28:47 +0200

    next try

--- simple-pp-react/client/main.jsx
+++ simple-pp-react/client/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
-
+// opps try something for a push // opps try something for a push 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
